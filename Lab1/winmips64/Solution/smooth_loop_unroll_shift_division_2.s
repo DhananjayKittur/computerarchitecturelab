c6879419b35f8c9fc3e53b1deaeb299e262820d1
Niroj Pokhrel <nirojpokhrel@gmail.com>
2016-05-13 19:26:40 +0200

    Latest and most optimized. Testing required

--- Lab1/winmips64/Solution/smooth_loop_unroll_shift_division_2.s
+++ Lab1/winmips64/Solution/smooth_loop_unroll_shift_division_2.s
@@ -1,9 +1,9 @@
 	.data
 N_COEFFS:   .word 3
-coeff: 		.double -1.0, 1.0, -0.2
-N_SAMPLES: .word 30
-sample:	.double 68, 23, 27, 7, 46, 54, 66, 100, 44, 65, 52, 96, 50, 93, 4, 1, 25, 5, 33, 44, 79, 28, 64, 30, 92, 0, 98, 18, 50, 58
-result:		.double 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+coeff: 		.double -0.9, 1.0, -0.9
+N_SAMPLES: .word 12
+sample:	.double 92, 60, 23, 78, 78, 90, 29, 56, 51, 22, 24, 89
+result:		.double 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 CR: .word32 0x10000
 DR:    .word32 0x10008
@@ -78,16 +78,15 @@ outer_loop:
 	mul.d f9, f1, f6
 	mul.d f10, f2, f7
 	mul.d f11, f3, f8
-	mov.d f5, f9
-	add.d f5, f5, f10
+	add.d f5, f9, f10
+	mov.d f6, f7
+	mov.d f7, f8
 	add.d f5, f5, f11
 	
 	daddi r7, r7, -8 
 	daddi r6, r6, -1 ;; Moved up so that the branch instruction doesnt have to wait for it
 	s.d f5, result(r7) ;;We will have to store in one place before the current pointer
 	daddi r7, r7, 16
-	mov.d f6, f7
-	mov.d f7, f8
 
 	bnez r6, outer_loop
 	
@@ -97,19 +96,19 @@ outer_loop:
 	l.d f4, sample(r7)
 	s.d f4, result(r7)
 
-printdouble:
-	lwu r11, CR(r0) ;; Control Register
-	lwu r12, DR(r0) ;; Data Register
-	daddi r10, r0, 3
-	daddi r7, r0, 0
-	ld r8, N_SAMPLES(r0)
-PRINT_LOOP:
-	l.d f1, result(r7)
-	s.d f1, (r12)   ;; output f0 .... 
-	sd r10, (r11)   ;; .... to screen
-	daddi r8, r8, -1
-	daddi r7, r7, 8
-	bnez r8, PRINT_LOOP
+;;printdouble:
+;;	lwu r11, CR(r0) ;; Control Register
+;;	lwu r12, DR(r0) ;; Data Register
+;;	daddi r10, r0, 3
+;;	daddi r7, r0, 0
+;;	ld r8, N_SAMPLES(r0)
+;;PRINT_LOOP:
+;;	l.d f1, result(r7)
+;;	s.d f1, (r12)   ;; output f0 .... 
+;;	sd r10, (r11)   ;; .... to screen
+;;	daddi r8, r8, -1
+;;	daddi r7, r7, 8
+;;	bnez r8, PRINT_LOOP
 	
 exit:
 	halt
